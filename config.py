3460d9ced5d0182e11f8441496d34ecbb67dc9e0
sahidin2-dot <dk0086425@gmail.com>
2025-03-14 00:41:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -76,10 +76,10 @@ logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[logging.StreamHandler()],
+    handlers=[logging.StreamHandler()
 )   
 RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
-logging.StreamHandler(),],
+logging.StreamHandler()
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
