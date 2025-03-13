6bb47b0d2e6df23b08a2ae2b8053c2014d633084
sahidin2-dot <dk0086425@gmail.com>
2025-03-14 01:15:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -77,7 +77,8 @@ logging.basicConfig(
     format="[%(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[logging.StreamHandler()]
-    
+)
+
 RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
 [logging.StreamHandler()
 ]
